8e9d653666e9b529b4c1ddea4f52d36a740f632a
AmandineJala <amandine.jala@kcl.ac.uk>
2016-02-02 23:48:46 +0000

    Adding my name to the contributors file

--- PRACoursework/src/Contributors.java
+++ PRACoursework/src/Contributors.java
@@ -1,10 +1,12 @@
 
 public class Contributors {
 
-		public static void main(String[] args){
-			
-			System.out.println("Eve Gazay");
-			
-			System.out.println("Nina Hasebe");
-		}
+	public static void main(String[] args){
+
+		System.out.println("Eve Gazay");
+
+		System.out.println("Nina Hasebe");
+
+		System.out.println("Amandine Jala");
+	}
 }
