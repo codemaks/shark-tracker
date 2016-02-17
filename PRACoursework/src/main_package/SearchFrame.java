aed5de9c123d3a0e038adf1d10a1464a0435b980
AmandineJala <amandine.jala@kcl.ac.uk>
2016-02-17 17:48:00 +0000

    Refactored SearchFrame with createWidgets() method Created a Main class Moved /doc/ outside the sources directory Added a .gitignore file

--- PRACoursework/src/main_package/SearchFrame.java
+++ PRACoursework/src/main_package/SearchFrame.java
@@ -19,55 +19,58 @@ import java.awt.event.ActionEvent;
 
 
 public class SearchFrame extends JFrame implements ActionListener{
-	
-		 int k = 3;
-		 JComboBox jcbstage_of_life = new JComboBox();
-		 JComboBox jcbtracking_range = new JComboBox();
-		 JComboBox jcbgender = new JComboBox();
-		 JComboBox jcbtag_location = new JComboBox();
-		 JButton search;
-		 JPanel mPanel = new JPanel(new BorderLayout());//main panel
-		 JPanel msPanel = new JPanel();//main south Panel (by opposition to the main panel)
-		 JPanel mwestPanel = new JPanel(new BorderLayout());//west Panel in main panel
-		 JPanel mwnorthPanel = new JPanel(new GridLayout(10,1));//north Panel in west panel which is in the main panel
-		 private Dimension westdim = new Dimension (300,this.getHeight());
-		 JPanel mcenterPanel = new JPanel(new GridLayout(k,1));//center Panel in main panel
 	 
 	 public SearchFrame() {
-		 
 		 super("Search");
-		 search = new JButton("Search");
-		 
-		 search.addActionListener(new ActionListener(){
-			 //anonymous inner class...
-			@Override
-			public void actionPerformed(ActionEvent e){
-						//1. read selected constraint from combo box
-					
-						//2. get all shark components
-						
-						//3. apply constraint on West panel filled with Shark Component objects
-						
-				 }
-			 
-		 });
-		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 
-		 this.setLayout(new BorderLayout());
-		 this.add(msPanel,BorderLayout.SOUTH);
-		 msPanel.setPreferredSize(new Dimension(WIDTH,50));
-		 msPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		 this.setPreferredSize(new Dimension(1200,700));
-		 this.add(mPanel);
-		 mPanel.add(mcenterPanel);
-		 mcenterPanel.add(new JScrollPane(new JTextArea()));
-		  
-		 mwestPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		 mwestPanel.setPreferredSize(westdim);
-		 
-		 mPanel.add(mwestPanel,BorderLayout.WEST);
-		 // The commented code below was used to test a very simplistic version of the west side of the frame.
-		 // You are welcome to uncomment it if necessary.
+		 createWidgets();
+	 }
+
+	private void createWidgets() {
+		JComboBox jcbstage_of_life = new JComboBox();
+		JComboBox jcbtracking_range = new JComboBox();
+		JComboBox jcbgender = new JComboBox();
+		JComboBox jcbtag_location = new JComboBox();
+
+		JPanel mPanel = new JPanel(new BorderLayout());//main panel
+		JPanel msPanel = new JPanel();//main south Panel (by opposition to the main panel)
+		JPanel mwestPanel = new JPanel(new BorderLayout());//west Panel in main panel
+		JPanel mwnorthPanel = new JPanel(new GridLayout(10,1));//north Panel in west panel which is in the main panel
+		JPanel mcenterPanel = new JPanel(new GridLayout(3,1));//center Panel in main panel
+
+		Dimension westdim = new Dimension (300,this.getHeight());
+
+		JButton search = new JButton("Search");
+
+		search.addActionListener(new ActionListener(){
+            //anonymous inner class...
+           @Override
+           public void actionPerformed(ActionEvent e){
+                       //1. read selected constraint from combo box
+
+                       //2. get all shark components
+
+                       //3. apply constraint on West panel filled with Shark Component objects
+
+                }
+
+        });
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setLayout(new BorderLayout());
+		this.add(msPanel,BorderLayout.SOUTH);
+		msPanel.setPreferredSize(new Dimension(WIDTH,50));
+		msPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.setPreferredSize(new Dimension(1200,700));
+		this.add(mPanel);
+		mPanel.add(mcenterPanel);
+		mcenterPanel.add(new JScrollPane(new JTextArea()));
+
+		mwestPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		mwestPanel.setPreferredSize(westdim);
+
+		mPanel.add(mwestPanel,BorderLayout.WEST);
+		// The commented code below was used to test a very simplistic version of the west side of the frame.
+		// You are welcome to uncomment it if necessary.
 //		 mwestPanel.add(mwnorthPanel);
 //		 mwnorthPanel.add(new JLabel("stage of life"));
 //		 mwnorthPanel.add(jcbstage_of_life);
@@ -78,18 +81,15 @@ public class SearchFrame extends JFrame implements ActionListener{
 //		 mwnorthPanel.add(new JLabel("tag location:"));
 //		 mwnorthPanel.add(jcbtag_location);
 //		 mwnorthPanel.add(search);
-		
-		 this.pack();
-	 }
-	 	
-		 public static void main(String[] args) {
-		JFrame frame = new SearchFrame();
-		frame.setVisible(true);
-	 }
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			
-		}
+		this.pack();
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
