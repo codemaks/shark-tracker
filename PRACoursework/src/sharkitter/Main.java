2b9320872cf593d462d0ea467a8633d3fdc39093
nhasebe <nina.hasebe@kcl.ac.uk>
2016-03-03 12:54:19 +0000

    Added image to search frame Refactored code

--- PRACoursework/src/sharkitter/Main.java
+++ PRACoursework/src/sharkitter/Main.java
@@ -1,6 +1,7 @@
 package sharkitter;
 
 import sharkitter.view.MenuFrame;
+import sharkitter.view.SearchFrame;
 
 import javax.swing.*;
 import java.io.File;
@@ -10,5 +11,8 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new MenuFrame();
         frame.setVisible(true);
+
+        SearchFrame sf = new SearchFrame();
+        sf.setVisible(true);
     }
 }
--- PRACoursework/src/sharkitter/view/SearchFrame.java
+++ PRACoursework/src/sharkitter/view/SearchFrame.java
@@ -23,8 +23,8 @@ public class SearchFrame extends JFrame implements Observer {
 
 	private JPanel centralPanel;
     private JPanel mWestPanel;
-    private JPanel mwNorthPanel;
-    private JPanel mwSouthPanel;
+
+    private JButton search;
 
 	private int counter;
 
@@ -33,94 +33,88 @@ public class SearchFrame extends JFrame implements Observer {
         jawsApi = new Jaws("EkZ8ZqX11ozMamO9", "E7gdkwWePBYT75KE", true);
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(1200, 700));
-        createWidgets();
+        createPanels();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
 	/**
 	 * Create and display the widgets on the main Frame
 	 */
-	private void createWidgets() {
-		mWestPanel = new JPanel(new BorderLayout());//west Panel in main panel
-		mwNorthPanel = new JPanel(new GridLayout(10, 1));//north Panel in west panel which is in the main panel
-        mwSouthPanel = new JPanel(new GridLayout(1, 1));//south panel in main west panel
-
-		//Dimension westDim = new Dimension(300, this.getHeight());
-
-		JButton search = new JButton("Search");
-
-		search.addActionListener(new ActionListener() {
-            //assuming we don't need this bit as SearchButtonListener is now a separate class?? <Nina>
-			@Override
-			public void actionPerformed(ActionEvent e){
-				//1. read selected constraint from combo box
-				String sOfLife = (String)stage_of_life.getSelectedItem();
-				String tRange = (String)tracking_range.getSelectedItem();
-				String gen = (String)gender.getSelectedItem();
-				String tagLoc = (String)tag_location.getSelectedItem();
-
-				//2. get all shark components by tracking range
-				if (sOfLife.equals("Last 24 hours")) {
-					System.out.println(sOfLife);
-					updateCentralPanel(jawsApi.past24Hours());
-				}
-                else if (sOfLife.equals("Last Week")) {
-					updateCentralPanel(jawsApi.pastWeek());
-				}
-                else if (sOfLife.equals("Last Month")) {
-					updateCentralPanel(jawsApi.pastMonth());
-				}
-                else {
-					System.out.println("Search ButtonListener Error 1 : Invalid ComboBox input");
-				}
-				//3. apply constraint on West panel filled with Shark Component objects
-
-            }
-        });
-
+	private void createPanels() {
+        createCentralPanel();
+        createWestPanel();
         createSouthPanel();
-		createCentralPanel();
 
-		mWestPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		//mWestPanel.setPreferredSize(westDim);
-        mWestPanel.setPreferredSize(new Dimension(300, this.getHeight()));
+        createSearchButton();
+        createComboBoxes();
 
-		add(mWestPanel,BorderLayout.WEST);
-
-		stage_of_life = new JComboBox();
-		stage_of_life.addItem("Any");
-		stage_of_life.addItem("Mature");
-		stage_of_life.addItem("Immature");
-		stage_of_life.addItem("Undetermined");
-
-		tracking_range = new JComboBox();
-		tracking_range.addItem("Last 24 Hours");
-		tracking_range.addItem("Last Week");
-		tracking_range.addItem("Last Month");
-
-		gender = new JComboBox();
-		gender.addItem("Any");
-		gender.addItem("Male");
-		gender.addItem("Female");
-
-		tag_location = new JComboBox();
-		for(String tagloc: jawsApi.getTagLocations()){
-			tag_location.addItem(tagloc);
-		}
-
-		mWestPanel.add(mwNorthPanel);
-		mwNorthPanel.add(new JLabel("Stage of life"));
-		mwNorthPanel.add(stage_of_life);
-		mwNorthPanel.add(new JLabel("Tracking range:"));
-		mwNorthPanel.add(tracking_range);
-		mwNorthPanel.add(new JLabel("Gender:"));
-		mwNorthPanel.add(gender);
-		mwNorthPanel.add(new JLabel("Tag location:"));
-		mwNorthPanel.add(tag_location);
-		mwNorthPanel.add(search);
+        createWNorthPanel();
+        createWSouthPanel();
 
 		pack();
 	}
+
+    /**
+     * Creates the combo boxes.
+     */
+    private void createComboBoxes() {
+        stage_of_life = new JComboBox();
+        stage_of_life.addItem("Any");
+        stage_of_life.addItem("Mature");
+        stage_of_life.addItem("Immature");
+        stage_of_life.addItem("Undetermined");
+
+        tracking_range = new JComboBox();
+        tracking_range.addItem("Last 24 Hours");
+        tracking_range.addItem("Last Week");
+        tracking_range.addItem("Last Month");
+
+        gender = new JComboBox();
+        gender.addItem("Any");
+        gender.addItem("Male");
+        gender.addItem("Female");
+
+        tag_location = new JComboBox();
+        for(String tagLoc: jawsApi.getTagLocations()){
+            tag_location.addItem(tagLoc);
+        }
+    }
+
+    /**
+     * Creates search button.
+     */
+    private void createSearchButton() {
+        search = new JButton("Search");
+
+        search.addActionListener(new ActionListener() {
+            //assuming we don't need this bit as SearchButtonListener is now a separate class?? <Nina>
+            @Override
+            public void actionPerformed(ActionEvent e){
+                //1. read selected constraint from combo box
+                String sOfLife = (String)stage_of_life.getSelectedItem();
+                String tRange = (String)tracking_range.getSelectedItem();
+                String gen = (String)gender.getSelectedItem();
+                String tagLoc = (String)tag_location.getSelectedItem();
+
+                //2. get all shark components by tracking range
+                if (sOfLife.equals("Last 24 hours")) {
+                    System.out.println(sOfLife);
+                    updateCentralPanel(jawsApi.past24Hours());
+                }
+                else if (sOfLife.equals("Last Week")) {
+                    updateCentralPanel(jawsApi.pastWeek());
+                }
+                else if (sOfLife.equals("Last Month")) {
+                    updateCentralPanel(jawsApi.pastMonth());
+                }
+                else {
+                    System.out.println("Search ButtonListener Error 1 : Invalid ComboBox input");
+                }
+                //3. apply constraint on West panel filled with Shark Component objects
+
+            }
+        });
+    }
 
 	/**
 	 * Create and display the central element of the SearchFrame i.e. the search results.
@@ -166,13 +160,39 @@ public class SearchFrame extends JFrame implements Observer {
      * Creates the west panel within the main panel.
      */
     private void createWestPanel() {
+        mWestPanel = new JPanel(new BorderLayout());//west Panel in main panel
 
+        mWestPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        mWestPanel.setPreferredSize(new Dimension(300, this.getHeight()));
+
+        add(mWestPanel,BorderLayout.WEST);
+    }
+
+    /**
+     * Creates the north panel within the west panel and adds it to the west panel.
+     */
+    private void createWNorthPanel() {
+        JPanel mwNorthPanel = new JPanel(new GridLayout(10, 1));
+
+        mwNorthPanel.add(new JLabel("Stage of life"));
+        mwNorthPanel.add(stage_of_life);
+        mwNorthPanel.add(new JLabel("Tracking range:"));
+        mwNorthPanel.add(tracking_range);
+        mwNorthPanel.add(new JLabel("Gender:"));
+        mwNorthPanel.add(gender);
+        mwNorthPanel.add(new JLabel("Tag location:"));
+        mwNorthPanel.add(tag_location);
+        mwNorthPanel.add(search);
+
+        mWestPanel.add(mwNorthPanel);
     }
 
     /**
      * Creates the south panel within the west panel and adds it to the west panel.
      */
-    private void createWestSouthPanel() {
+    private void createWSouthPanel() {
+        JPanel mwSouthPanel = new JPanel(new GridLayout(1, 1));
+
         ImageIcon shark = new ImageIcon(getClass().getClassLoader().getResource("resources/shark9.png"));
         Image img = shark.getImage();
         Image newImg = img.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
