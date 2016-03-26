1ab38cf66ef1e32d7aa6aba6fa2a0147c667daac
Eve <eve.gazay@kcl.ac.uk>
2016-03-26 18:38:47 +0000

    Commented JawsApi

--- PRACoursework/src/sharkitter/api/JawsApi.java
+++ PRACoursework/src/sharkitter/api/JawsApi.java
@@ -2,9 +2,16 @@ package sharkitter.api;
 
 import api.jaws.Jaws;
 
+    /**
+     * Singleton class which can only return an instance of the Jaws class.
+     */
 public class JawsApi {
     private static Jaws instance;
 
+    /**
+     * static method returning an instance of the Jaws class from the api if there is any, otherwise returns a new Jaws.
+     * @return instance
+     */
     public static Jaws getInstance() {
         if(instance == null) {
             instance = new Jaws("EkZ8ZqX11ozMamO9", "E7gdkwWePBYT75KE", true);
